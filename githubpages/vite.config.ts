e061c722f7c58dc6297bf254531a450b840a9acb
Bailey Sostek <baileysostek@users.noreply.github.com>
2025-05-28 22:48:49 -0400

    Removed duplicate /baileysostek

--- githubpages/vite.config.ts
+++ githubpages/vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/baileysostek/',
   plugins: [react()],
   build: {
     outDir: '../docs',
